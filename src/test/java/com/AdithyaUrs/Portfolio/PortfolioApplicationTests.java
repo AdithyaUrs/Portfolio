a0d51deab20d822d6a369d95a27ee3a7a6949aaa
Adithya Urs <adithyaurs.2001@gmail.com>
2025-06-06 22:58:25 +0530

    first commit

---
+++ src/test/java/com/AdithyaUrs/Portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdithyaUrs.Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
